892bd96ff962ecd3bcdf090279089bd58c9fd621
Orlando Rodrigues <orlando.r.s@outlook.com>
2021-07-10 14:51:18 -0300

    Updated to make person and employee abstract

--- src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -2,20 +2,7 @@ package one.digitalinnovation.collections
 
 
 
-class Pessoa {//alter class
-    var nome: String = "Orlando"
-    var cpf: String = "123.123.123-12"
-    private set
-
-    constructor()
-
-    fun nomecpf() = "Nome: $nome CPF: $cpf"
-}
-
-fun main() {
-    val orlando = Pessoa()
-
-println(orlando.nomecpf())
-
-
-}
+abstract class Pessoa (//alter class
+ val nome: String,
+ val cpf: String
+)
